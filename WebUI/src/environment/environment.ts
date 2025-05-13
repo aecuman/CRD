c253b15c91b3728213fa2599e82f3d07b42c75e4
Ecuman Anthony Stanley <aecuman24@gmail.com>
2025-05-13 15:45:14 +0300

    changing api Url for prod

--- WebUI/src/environment/environment.ts
+++ WebUI/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl:'https://localhost:7259',//https://crdapi.azurewebsites.net/',//'https://localhost:44328',////'https://api.redahug.com:444/redah-core-prod',//   
+    baseUrl:'http://localhost:5100',//https://crdapi.azurewebsites.net/',//'https://localhost:44328',////'https://api.redahug.com:444/redah-core-prod',//   
     version:'v1'
   };
